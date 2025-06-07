296b6e2721a21f59d202d4c23e9e5a44ffb70481
Andrew <andrewfreiria28@gmail.com>
2025-06-07 20:03:53 -0300

    feat: implementa método de conexao na classe Conexao

--- src/controller/Conexao.java
+++ src/controller/Conexao.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-/**
- *
- * @author Micro
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexao {
+    
+    // PARÂMETROS
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "12345";
+    private static final String URL = "jdbc:postgresql://127.0.0.1:5432/EscolaIdiomas";
+    private static final String DRIVER = "org.postgresql.Driver";
+    
+    // Estabelecer a conexão entre o APP e o SGBD
+    public static Connection conectar() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (Exception e){
+            System.err.println("ERRO: " + e.getMessage());
+            return null;
+        }
+    }
     
 }
