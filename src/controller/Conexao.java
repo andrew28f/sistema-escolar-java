731daae325be6e6936a172bf268feec2ccbfaa10
Andrew <andrewfreiria28@gmail.com>
2025-06-07 20:05:32 -0300

    fix: corrige senha de acesso ao banco

--- src/controller/Conexao.java
+++ src/controller/Conexao.java
@@ -7,7 +7,7 @@ public class Conexao {
     
     // PARÂMETROS
     private static final String USUARIO = "postgres";
-    private static final String SENHA = "12345";
+    private static final String SENHA = "password12345";
     private static final String URL = "jdbc:postgresql://127.0.0.1:5432/EscolaIdiomas";
     private static final String DRIVER = "org.postgresql.Driver";
     
