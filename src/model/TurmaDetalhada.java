a4ad62abbf10eb50fe8176b15ab5a7de63ba1949
Andrew <andrewfreiria28@gmail.com>
2025-06-09 15:52:46 -0300

    feat(TurmaDetalhada): implementa classe com construtor e getters

--- src/model/TurmaDetalhada.java
+++ src/model/TurmaDetalhada.java
@@ -2,4 +2,44 @@ package model;
 
 public class TurmaDetalhada {
     
+    private int id;
+    private String nome;
+    private String nomeCurso;
+    private String nomeNivel;
+    private String nomeProfessor;
+    private String horario;
+
+    public TurmaDetalhada(int id, String nome, String nomeCurso, String nomeNivel, String nomeProfessor, String horario) {
+        this.id = id;
+        this.nome = nome;
+        this.nomeCurso = nomeCurso;
+        this.nomeNivel = nomeNivel;
+        this.nomeProfessor = nomeProfessor;
+        this.horario = horario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public String getNomeNivel() {
+        return nomeNivel;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+    
 }
