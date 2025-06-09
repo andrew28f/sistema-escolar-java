06401a66924407e7a6e0fb4050ca06ecefc22521
Andrew <andrewfreiria28@gmail.com>
2025-06-09 00:24:20 -0300

    feat: implementa a classe Usuario com atributos, construtor, setters e getters

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Micro
- */
 public class Usuario {
+    
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario() {
+        this(0, "", "", "");
+    }
+
+    public Usuario(int id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Id: %d\nNome: %s\nEmail: %s\nSenha: %s\n", id, nome, email, senha);
+    }
     
 }
