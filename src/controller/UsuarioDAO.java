947ab67580a630a594e7362784f2f6d6fdd7ee23
Andrew <andrewfreiria28@gmail.com>
2025-06-09 00:58:06 -0300

    feat(UsuarioDAO): adiciona método inserir()

--- src/controller/UsuarioDAO.java
+++ src/controller/UsuarioDAO.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-/**
- *
- * @author Micro
- */
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.Usuario;
+
 public class UsuarioDAO {
+    
+    private Connection con;
+    private PreparedStatement cmd;
+    
+    public UsuarioDAO() {
+        con = Conexao.conectar();
+    }
+    
+    public int inserir(Usuario u) {
+        try {
+            
+            // Instrução SQL para inserção de dados
+            String sql = "insert into tb_usuario(nome, email, senha) values (?, ?, MD5(?))";
+
+            // Verificação da sintaxe do comando sql
+            cmd = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            // Especificação de parâmetros da instrução sql
+            cmd.setString(1, u.getNome());
+            cmd.setString(2, u.getEmail());
+            cmd.setString(3, u.getSenha());
+            
+            // Execução da instrução sql
+            if (cmd.executeUpdate() > 0) {
+                // Retorna chave primária (ID) do usuário
+                ResultSet rs = cmd.getGeneratedKeys();
+                return rs.next() ? rs.getInt(1) : -1;
+            }
+            
+            // Retorna -1 se algo der errado
+            return -1;
+            
+        } catch (Exception e) {
+            System.err.println("ERRO: " + e.getMessage());
+            return -1;
+        }
+    }
     
 }
