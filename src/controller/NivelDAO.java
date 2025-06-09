acb1cd0bdc84d511fd8805bb8c719b6e86dea86e
Andrew <andrewfreiria28@gmail.com>
2025-06-09 01:50:46 -0300

    feat(NivelDAO): adiciona metodo inserir()

--- src/controller/NivelDAO.java
+++ src/controller/NivelDAO.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-/**
- *
- * @author Micro
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import model.Nivel;
+
 public class NivelDAO {
+    
+    private Connection con;
+    private PreparedStatement cmd;
+    
+    public NivelDAO() {
+        con = Conexao.conectar();
+    }
+    
+    public int inserir(Nivel n) {
+        try {
+            String sql = "insert into tb_nivel(nome, descricao, requisitos)"
+                    + "values (?, ?, ?)";
+            
+            cmd = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            cmd.setString(1, n.getNome());
+            cmd.setString(2, n.getDescricao());
+            cmd.setString(3, n.getRequisitos());
+            
+            if (cmd.executeUpdate() > 0) {
+                ResultSet rs = cmd.getGeneratedKeys();
+                return rs.next() ? rs.getInt(1) : -1;
+            }
+            
+            return -1;
+            
+        } catch (Exception e) {
+            System.err.println("ERRO: " + e.getMessage());
+            return -1;
+        }
+    }
     
 }
