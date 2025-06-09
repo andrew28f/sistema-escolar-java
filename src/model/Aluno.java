6f2cfe9fb4efd8c40774344dade17d583a6335fb
Andrew <andrewfreiria28@gmail.com>
2025-06-08 23:17:57 -0300

    feat: implementa classe Aluno

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Micro
- */
 public class Aluno {
     
+    private String nome;
+    private String email;
+    private String telefone;
+    private String dataNascimento;
+
+    public Aluno() {
+        this("","","","");
+    }
+
+    public Aluno(String nome, String email, String telefone, String dataNascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nome: %s\nEmail: %s\nTelefone: %s\nData de Nascimento: %s\n", nome, email, telefone, dataNascimento);
+    }
 }
