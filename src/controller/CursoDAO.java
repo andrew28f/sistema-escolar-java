cee7716d6f8d7273bb7d05cc12be53b1a7beef79
Andrew <andrewfreiria28@gmail.com>
2025-06-09 01:50:28 -0300

    feat(CursoDAO): adiciona metodo inserir()

--- src/controller/CursoDAO.java
+++ src/controller/CursoDAO.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-/**
- *
- * @author Micro
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import model.Curso;
+
 public class CursoDAO {
+    
+    private Connection con;
+    private PreparedStatement cmd;
+    
+    public CursoDAO() {
+        con = Conexao.conectar();
+    }
+    
+    public int inserir(Curso c) {
+        try {
+            String sql = "insert into tb_curso(nome, carga_horaria, descricao)"
+                    + "values (?, ?, ?)";
+            
+            cmd = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            cmd.setString(1, c.getNome());
+            cmd.setInt(2, c.getCargaHoraria());
+            cmd.setString(3, c.getDescricao());
+            
+            if (cmd.executeUpdate() > 0) {
+                ResultSet rs = cmd.getGeneratedKeys();
+                return rs.next() ? rs.getInt(1) : -1;
+            }
+            
+            return -1;
+            
+        } catch (Exception e) {
+            System.err.println("ERRO: " + e.getMessage());
+            return -1;
+        }
+    }
     
 }
